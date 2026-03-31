9b3fffb3dd4fbc1fed9315ebc457478954610954
adityajain0309pro-cmyk <adityajain0309pro@gmail.com>
2026-03-31 21:36:32 +0530

    UC1: Application entry and welcome message implemented

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+/**
+ * Book My Stay Application - Use Case 1
+ * This class represents the entry point of the Hotel Booking System.
+ * It prints a welcome message along with application details.
+ *
+ * @author Aditya Jain
+ * @version 1.0
+ */
+public class UseCase1HotelBookingApp {
+
+    /**
+     * Main method - Entry point of the program
+     * @param args Command line arguments
+     */
     public static void main(String[] args) {
 
-        }
+        // Welcome Banner
+        System.out.println("=======================================");
+        System.out.println("      Welcome to Book My Stay App      ");
+        System.out.println("=======================================");
+
+        // Application Details
+        System.out.println("Application Name : Hotel Booking System");
+        System.out.println("Version          : 1.0");
+
+        // Status Message
+        System.out.println("\nSystem initialized successfully!");
+        System.out.println("Ready for next use cases...");
     }
+}
